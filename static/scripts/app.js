4d447f31a0c7b9198e51f8d60cfca821971214b3
Scott Henshaw <shenshaw@vfs.com>
2018-10-29 12:25:20 -0700

    Update to a more modern JS module.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -22,57 +22,16 @@
  * 
  */
 'use strict';
-(function() {
-    
-class NameSpace {
-    constructor() {
-        this.data = new WeakMap();
-    }
-
-    members( key, value = undefined ) {
-        if (value != undefined) {
-            this.data.set( key, value );
-        }
-        return this.data.get( key );
-    }
-
-    static resultFromData( data ) {
-
-        let result = null; 
-        switch (typeof data) {
-            case 'string':
-                result = $.parseJSON( data );
-                break;
-                
-            case 'object':
-                result = data;
-                break;
-            
-            default:
-                result = null; 
-                break;
-        }
-        return result;
-    }
-}
-
-if (app === undefined) {
-    var app = {
-       'private': new NameSpace(),
-       resultFromData: NameSpace.resultFromData
-    }
-}
-
 
 class App {
 
     constructor() {
         
         // the local object contains all the private members used in this class             
-        let m = app.private.members( this, {
+        this.__private__ = {
             playerList: [],
             done:       false
-        });
+        };
         
         // Do some initialization of the member variables for the app
         this.populatePlayerList();            
@@ -80,38 +39,34 @@ class App {
         // Create controllers to manage model objects and link them to DOM
         // view elements
         
-        // Define the Event handlers for the app
-        
+        // Define the Event handlers for the app        
     }   
         
-    run() {
-      
+    run() {      
         // update something
         // render something
     }
     
-    populatePlayerList() {
-        let m = app.private.members( this );
+    async populatePlayerList() {
+        let m = this.__private__;
         
     	// Post request for data to the server (assuming GAE server)
         let instr = $.param({'cmd':'get_player_data'});
-        $.post( '/', instr )
-            .then( ( data ) => {
+        let data = await $.post( '/', instr );
                 
-                let result = app.resultFromData( data );
-                if (result.returnCode === 0) {
-    
-                    // use the data inside result, each member will match the dictionary
-                    // from the server
-                    m.playerList = result.players;
-    
-                } else if (result.returnCode === 99) {
-    
-                    // handle error returned by the server
-                    window.location = '/';
-                    return;        
-                }
-            });
+        let result = this.resultFromData( data );
+        if (result.returnCode === 0) {
+
+            // use the data inside result, each member will match the dictionary
+            // from the server
+            m.playerList = result.players;
+
+        } else if (result.returnCode === 99) {
+
+            // handle error returned by the server
+            window.location = '/';
+            return;        
+        }
     }
     
     resultFromData( data ) {
@@ -134,10 +89,8 @@ class App {
     }
 }
 
-$(document).ready( function() {
+// Main
+$(document).ready( event => {
 
     let app = new App();            
 });
-
-
-})();
